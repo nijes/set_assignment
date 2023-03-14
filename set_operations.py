aad85bb516f374a58d4bb754edece085b0ab72be
Flitto <flitto@Flittoui-MacBookPro-2.local>
2023-03-14 12:24:04 +0900

    check basic operation

--- set_operations.py
+++ set_operations.py
@@ -1,13 +1,14 @@
 import argparse
+import pandas as pd
 
-# python3 set_operations.py -p ~/project/data -o output.xlsx -i sample_1.xlsx sample_2.xlsx --intersection --key_columns group_id en_id --for_any_key_columns
+
 def get_arguments():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-p", "--project", dest="project", action="store", required=True)
-    parser.add_argument("-o", "--output", dest="output", action="store", required=True)
-    parser.add_argument("-i", "--input", dest="input", nargs="+", action="store", required=True)
+    parser.add_argument("-p", "--project", dest="project_dir", action="store", required=True)
+    parser.add_argument("-o", "--output", dest="output_file", action="store", required=True)
+    parser.add_argument("-i", "--input", dest="input_files", nargs="+", action="store", required=True)
 
     parser.add_argument("--diff", dest="operation", action="store_const", const="diff", required=False)
     parser.add_argument("--intersection", dest="operation", action="store_const", const="intersection", required=False)
@@ -22,9 +23,41 @@ def get_arguments():
     #print(args.operation)
     #print(args.key_columns)
 
-    return args.project, args.input, args.output, args.operation, args.key_columns, args.any
+    return args.project_dir, args.input_files, args.output_file, args.operation, args.key_columns, args.any
+
+
+def main(project_dir, input_files, operation, key_columns):
+
+    df1 = pd.read_excel(project_dir + '/' + input_files[0])
+    df2 = pd.read_excel(project_dir + '/' + input_files[1])
+    
+    key_column = key_columns[0]
+    #print(key_column)
+
+    # col_to_set
+    df1_set = set(df1[key_column])
+    df2_set = set(df2[key_column])
+
+    if operation == 'intersection':
+        # intersection
+        inter_df = df1[df1[key_column].isin(df1_set&df2_set)]
+        return inter_df
+
+    elif operation == 'diff':
+        # diff
+        diff_df1 = df1[df1[key_column].isin(df1_set-df2_set)]
+        diff_df2 = df2[df2[key_column].isin(df2_set-df1_set)]
+        diff_df = pd.concat([diff_df1, diff_df2])
+        return diff_df
+
+    elif operation == 'union':
+        # union
+        diff_df2 = df2[df2[key_column].isin(df2_set-df1_set)]
+        union_df = pd.concat([df1, diff_df2])
+        return union_df
 
 
 
 if __name__ == "__main__":
-    project, input, output, operation, key_columns, any = get_arguments()
+    project_dir, input_files, output_file, operation, key_columns, any = get_arguments()
+    print( main(project_dir, input_files, operation, key_columns) )
