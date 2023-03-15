e63f5ff5be19d6ca0780d4f1bf9dd0b0cc46769c
nijes <monochrome05@gmail.com>
2023-03-15 16:23:58 +0900

    rearrange main function

--- set_operations.py
+++ set_operations.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+from setoperator import PandasOperator
 
 
 def get_arguments():
@@ -23,66 +24,33 @@ def get_arguments():
     return args.project_dir, args.input_files, args.output_file, args.operation, args.key_columns, args.for_any
 
 
+def main(df_list, operation, key_columns, for_any):
 
-def main(project_dir, input_files, operation, key_columns, for_any):
-
-    df1 = pd.read_excel(project_dir + '/' + input_files[0])
-    df2 = pd.read_excel(project_dir + '/' + input_files[1])
-
-
-    #operation = 'union'
-    #for_any = True
+    pdOperator = PandasOperator()
 
     if operation == 'intersection':
-        if for_any:
-            for key_column in key_columns:
-                df1_set = set(df1[key_column])
-                df2_set = set(df2[key_column])
-                if key_column == key_columns[0]:
-                    inter_df = df1[df1[key_column].isin(df1_set&df2_set)]
-                else:
-                    inter_df = pd.concat([inter_df, df1[df1[key_column].isin(df1_set&df2_set)]], ignore_index=True).drop_duplicates()
-        else:
-            for key_column in key_columns:
-                df1_set = set(df1[key_column])
-                df2_set = set(df2[key_column])
-                df1 = df1[df1[key_column].isin(df1_set&df2_set)]
-                df2 = df2[df2[key_column].isin(df1_set&df2_set)]
-            inter_df = df1
-        return inter_df
-        
-
-    elif operation == 'diff': #다시해야함
-        if for_any:
-            for key_column in key_columns:
-                df1_set = set(df1[key_column])
-                df2_set = set(df2[key_column])
-                if key_column == key_columns[0]:
-                    diff_df = df1[df1[key_column].isin(df1_set-df2_set)]
-                else:
-                    diff_df = pd.concat([diff_df, df1[df1[key_column].isin(df1_set-df2_set)]]).drop_duplicates()
-        else:
-            for key_column in key_columns:
-                df1_set = set(df1[key_column])
-                df2_set = set(df2[key_column])
-                df1 = df1[df1[key_column].isin(df1_set-df2_set)]
-                df2 = df2[df2[key_column].isin(df2_set-df1_set)]
-            diff_df = df1
-        return diff_df
-                
-
+        return pdOperator.intersection(df_list, key_columns=key_columns, for_any=for_any)
+    elif operation == 'diff':
+        return pdOperator.diff(df_list, key_columns=key_columns, for_any=for_any)
     elif operation == 'union':
-        union_df = pd.concat([df1, df2])
-        if for_any:
-            union_df = union_df.drop_duplicates(subset=key_columns)
-        else:
-            for key_column in key_columns:
-                union_df = union_df.drop_duplicates(subset=[key_column])
-        return union_df
+        return pdOperator.union(df_list, key_columns=key_columns, for_any=for_any)
+    
+    
+def readFiles(project_dir, input_files):
+    return [ pd.read_excel(project_dir + '/' + file) for file in input_files ]
+
+
+def saveFile(df, output_file):
+    df.to_excel(output_file, index=False)
 
 
 
 if __name__ == "__main__":
+    
     project_dir, input_files, output_file, operation, key_columns, for_any = get_arguments()
-    #main(project_dir, input_files, operation, key_columns, for_any).to_excel(output_file, index=False)
-    print( main(project_dir, input_files, operation, key_columns, for_any) )
+
+    result = main( readFiles(project_dir, input_files), operation, key_columns, for_any)
+    saveFile(result, output_file)
+
+    #print(result)
+    #print(len(result))
