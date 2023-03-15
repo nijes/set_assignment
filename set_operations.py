d0ca7aa0d912b82aa9cb0d784674cd5e301efa83
nijes <monochrome05@gmail.com>
2023-03-15 19:00:26 +0900

    edit readFiles function

--- set_operations.py
+++ set_operations.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 from setoperator import PandasOperator
+import json
 
 
 def get_arguments():
@@ -24,18 +25,54 @@ def get_arguments():
     return args.project_dir, args.input_files, args.output_file, args.operation, args.key_columns, args.for_any
 
 
-def main(df_list, operation, key_columns, for_any):
+def main(df_list:list[object], operation:str, key_columns:list[str], for_any:bool) -> object:
     pdoperator = PandasOperator(df_list, key_columns, for_any)
     return pdoperator(operation)
 
 
-def readFiles(project_dir, input_files):
-    return [ pd.read_excel(project_dir + '/' + file) for file in input_files ]
+def readFiles(project_dir:str, input_files:list[str]) -> list[object]:    
+    df_list = []
+    for file in input_files:
+        input_path = project_dir + '/' + file
+        # excel to df
+        if file.split('.')[-1] == 'xlsx':
+            df_list.append(pd.read_excel(input_path))
+        # csv to df
+        elif file.split('.')[-1] == 'csv':
+            df_list.append(pd.read_csv(input_path))
+        # tsv to df
+        elif file.split('.')[-1] == 'tsv':
+            df_list.append(pd.read_csv(input_path, sep='\t'))
+        # jsonl to df
+        elif file.split('.')[-1] == 'jsonl':
+            json_list = []
+            with open(input_path, 'r', encoding='utf-8') as f:
+                for line in f:
+                    json_list.append(json.loads(line.rstrip('\n|\r')))
+            df_cols = json_list[0].keys()
+            df_data = []
+            for d in json_list:
+                df_data.append([])
+                for col in df_cols:
+                    df_data[-1].append(d.get(col))
+            df_list.append(pd.DataFrame(df_data, columns=df_cols))
+        else:
+            raise Exception("지원하지 않는 파일 포맷")
+    return df_list
 
 
-def saveFile(df, output_file):
-    df.to_excel(output_file, index=False)
-
+def saveFile(df:object, output_file:str):
+    file_extension = output_file.split('.')[-1]
+    if file_extension == 'xlsx':
+        df.to_excel(output_file, index=False)
+    elif file_extension == 'csv':
+        pass
+    elif file_extension == 'tsv':
+        pass
+    elif file_extension == 'jsonl':
+        pass
+    else:
+        raise Exception("지원하지 않는 파일 포맷")
 
 
 if __name__ == "__main__":
@@ -43,7 +80,7 @@ if __name__ == "__main__":
     project_dir, input_files, output_file, operation, key_columns, for_any = get_arguments()
 
     result = main( readFiles(project_dir, input_files), operation, key_columns, for_any)
-    #saveFile(result, output_file)
+    saveFile(result, output_file)
 
-    print(result)
+    #print(result)
     print(len(result))
