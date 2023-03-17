5a429d6bf573f531f9abe904c30514b1d8539ce7
nijes <monochrome05@gmail.com>
2023-03-17 18:58:40 +0900

    edit function name

--- set_operations.py
+++ set_operations.py
@@ -1,20 +1,20 @@
 from setoperator import getargs, savedf, PandasOperator
 
 
-def main(project_dir:str, input_list:list[str], operation:str, key_columns:list[str], for_any:bool) -> object:
+def operationResult(path:str, input_list:list[str], operation:str, key_columns:list[str], for_any:bool) -> object:
     
-    pdoperator = PandasOperator(project_dir, input_list, key_columns, for_any)
+    pdoperator = PandasOperator(path, input_list, key_columns, for_any)
     result_df = pdoperator(operation)
 
     return result_df
 
 
 if __name__ == "__main__":
+    path, input_files, output_file, operation, key_columns, for_any = getargs()
     
-    project_dir, input_files, output_file, operation, key_columns, for_any = getargs()
+    result_df = operationResult( path, input_files, operation, key_columns, for_any)
     
-    result_df = main( project_dir, input_files, operation, key_columns, for_any)
-    output_path = f'{project_dir}/{output_file}'
+    output_path = f'{path}/{output_file}'
     savedf(result_df, output_path)
 
     #print(result_df)
