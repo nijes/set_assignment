3967b4c121d46d3dcf3f7225dcb0271e6c8bca21
nijes <monochrome05@gmail.com>
2023-03-17 16:43:23 +0900

    edit save method

--- set_operations.py
+++ set_operations.py
@@ -1,10 +1,12 @@
 from setoperator import getargs, savedf, PandasOperator
-from time import time
 
 
 def main(project_dir:str, input_list:list[str], operation:str, key_columns:list[str], for_any:bool) -> object:
+    
     pdoperator = PandasOperator(project_dir, input_list, key_columns, for_any)
-    return pdoperator(operation)
+    result_df = pdoperator(operation)
+
+    return result_df
 
 
 if __name__ == "__main__":
@@ -12,7 +14,8 @@ if __name__ == "__main__":
     project_dir, input_files, output_file, operation, key_columns, for_any = getargs()
     
     result_df = main( project_dir, input_files, operation, key_columns, for_any)
-    savedf(result_df, output_file)
+    output_path = f'{project_dir}/{output_file}'
+    savedf(result_df, output_path)
 
     #print(result_df)
     #print(len(result_df))
