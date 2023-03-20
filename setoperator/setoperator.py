f5be28ad345da71b3b45b01dcac94b3ce5687690
nijes <monochrome05@gmail.com>
2023-03-20 18:58:28 +0900

    make DaskOperator class

--- setoperator/setoperator.py
+++ setoperator/setoperator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import polars as pl
+import dask.dataframe as dd
 from .dataio import readdf
 from functools import reduce
 
@@ -11,17 +12,18 @@ class SetOperator:
         self.key_columns = key_columns
         self.for_any = for_any
 
-
-class PandasOperator(SetOperator):
-    def __init__(self, path:str, input_list:list[str], key_columns:list[str], for_any:bool):
-        super().__init__(path, input_list, key_columns, for_any)
-        self.dftype = 'pandas'
-
     def __call__(self, operation:str):
         if operation == 'intersection': return self._intersection()
         elif operation == 'diff': return self._diff()
         elif operation == 'union': return self._union()
         else: raise SyntaxError
+
+
+
+class PandasOperator(SetOperator):
+    def __init__(self, path:str, input_list:list[str], key_columns:list[str], for_any:bool):
+        super().__init__(path, input_list, key_columns, for_any)
+        self.dftype = 'pandas'
 
 
     def _intersection_condition(self, main_df:object, sub_df:object) -> object:
@@ -75,12 +77,6 @@ class PolarsOperator(SetOperator):
         super().__init__(path, input_list, key_columns, for_any)
         self.dftype = 'polars'
 
-    def __call__(self, operation:str):
-        if operation == 'intersection': return self._intersection()
-        elif operation == 'diff': return self._diff()
-        elif operation == 'union': return self._union()
-        else: raise SyntaxError
-
 
     def _intersection_condition(self, main_df:object, sub_df:object) -> object:
         condition_list = []
@@ -123,5 +119,56 @@ class PolarsOperator(SetOperator):
             sub_df = readdf(f'{self.path}/{input}', self.dftype)
             sub_df = sub_df.filter(~self._intersection_condition(sub_df, result_df))        
             result_df = pl.concat([result_df, sub_df], how='diagonal').select(result_df.columns)
+
+        return result_df
+    
+
+
+class DaskOperator(SetOperator):
+    def __init__(self, path:str, input_list:list[str], key_columns:list[str], for_any:bool):
+        super().__init__(path, input_list, key_columns, for_any)
+        self.dftype = 'dask'
+        # npartitions(파티션 수), num_workers(멀티프로세싱에 사용하는 코어 수) 입력 받기
+
+    def _intersection_condition(self, main_df:object, sub_df:object) -> object:
+        condition_list = []
+        for key_column in self.key_columns:
+            condition_list.append(main_df[key_column].isin(set(sub_df[key_column])))
+        
+        if self.for_any:
+            condition = reduce(lambda x, y: x | y, condition_list)
+        else:
+            condition = reduce(lambda x, y: x & y, condition_list)
+
+        return condition
+
+
+    def _intersection(self) -> object:
+        result_df = readdf(f'{self.path}/{self.input_list[0]}', self.dftype)
+
+        for input in self.input_list[1:]:
+            sub_df = readdf(f'{self.path}/{input}', self.dftype)
+            result_df = result_df[self._intersection_condition(result_df, sub_df)]
+
+        return result_df
+
+
+    def _diff(self) -> object:
+        result_df = readdf(f'{self.path}/{self.input_list[0]}', self.dftype)
+
+        for input in self.input_list[1:]:
+            sub_df = readdf(f'{self.path}/{input}', self.dftype)
+            result_df = result_df[~self._intersection_condition(result_df, sub_df)]
+        
+        return result_df       
+
+
+    def _union(self) -> object:
+        result_df = readdf(f'{self.path}/{self.input_list[0]}', self.dftype)
+
+        for input in self.input_list[1:]:
+            sub_df = readdf(f'{self.path}/{input}', self.dftype)
+            sub_df = sub_df[~self._intersection_condition(sub_df, result_df)]        
+            result_df = pd.concat([result_df, sub_df])[result_df.columns]
 
         return result_df
