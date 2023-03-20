bf26cf0f8a9c39d701c1d6f0fbbf6b8351a0e44b
nijes <monochrome05@gmail.com>
2023-03-20 18:57:23 +0900

    add dataio function by dask

--- setoperator/dataio.py
+++ setoperator/dataio.py
@@ -1,5 +1,6 @@
 import polars as pl
 import pandas as pd
+import dask.dataframe as dd
 
 
 def readdf(input_path:str, dftype:str) -> object:
@@ -15,8 +16,17 @@ def readdf(input_path:str, dftype:str) -> object:
         # jsonl to df
         elif file_extension == 'jsonl': return pl.read_ndjson(input_path)
         else: raise Exception("지원하지 않는 파일 포맷")
-    
-    else:
+    elif dftype == 'dask':
+        # excel to df
+        if file_extension == 'xlsx': return dd.from_pandas(pd.read_excel(input_path), npartitions=1)
+        # csv to df
+        elif file_extension == 'csv': return dd.read_csv(input_path)
+        # tsv to df
+        elif file_extension == 'tsv': return dd.read_csv(input_path, sep='\t')
+        # jsonl to df
+        elif file_extension == 'jsonl': return dd.read_json(input_path, lines=True)
+        else: raise Exception("지원하지 않는 파일 포맷")    
+    elif dftype == 'pandas':
         # excel to df
         if file_extension == 'xlsx': return pd.read_excel(input_path, dtype="object")
         # csv to df
@@ -26,6 +36,8 @@ def readdf(input_path:str, dftype:str) -> object:
         # jsonl to df
         elif file_extension == 'jsonl': return pd.read_json(input_path, lines=True, dtype="object")
         else: raise Exception("지원하지 않는 파일 포맷")
+    else:
+        raise Exception
 
 
 
@@ -42,7 +54,17 @@ def savedf(df:object, output_path:str, dftype:str): # 인자로 dftype 추가
         # df to jsonl
         elif file_extension == 'jsonl': df.write_ndjson(output_path)
         else: raise Exception("지원하지 않는 파일 포맷")
-    else:
+    if dftype == 'dask':
+        # df to excel
+        if file_extension == 'xlsx': df.compute().to_excel(output_path, index=False)
+        # df to csv
+        elif file_extension == 'csv': df.to_csv(output_path)
+        # df to tsv
+        elif file_extension == 'tsv': df.to_csv(output_path, sep='\t')
+        # df to jsonl
+        elif file_extension == 'jsonl': df.compute().to_json(output_path, orient='records', lines=True, force_ascii=False)
+        else: raise Exception("지원하지 않는 파일 포맷")
+    elif dftype == 'pandas':
         # df to excel
         if file_extension == 'xlsx': df.to_excel(output_path, index=False)
         # df to csv
@@ -52,3 +74,5 @@ def savedf(df:object, output_path:str, dftype:str): # 인자로 dftype 추가
         # df to jsonl
         elif file_extension == 'jsonl': df.to_json(output_path, orient='records', lines=True, force_ascii=False)
         else: raise Exception("지원하지 않는 파일 포맷")
+    else:
+        raise Exception
