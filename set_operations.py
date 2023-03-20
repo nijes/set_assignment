56b8a54349a85eb1908267984e928c97b84bf90e
nijes <monochrome05@gmail.com>
2023-03-20 19:02:58 +0900

    add polars&dask framework

--- set_operations.py
+++ set_operations.py
@@ -1,21 +1,30 @@
-from setoperator import getargs, savedf, PandasOperator
+from setoperator import getargs, savedf, PandasOperator, PolarsOperator, DaskOperator
+from time import time
 
+def operationResult(path:str, input_list:list[str], operation:str, key_columns:list[str], for_any:bool, dftype:str) -> object:
 
-def operationResult(path:str, input_list:list[str], operation:str, key_columns:list[str], for_any:bool) -> object:
-    
-    pdoperator = PandasOperator(path, input_list, key_columns, for_any)
-    result_df = pdoperator(operation)
+    if dftype == 'pandas':    
+        operator = PandasOperator(path, input_list, key_columns, for_any)
+    elif dftype == 'polars':
+        operator = PolarsOperator(path, input_list, key_columns, for_any)
+    elif dftype == 'dask':
+        operator = DaskOperator(path, input_list, key_columns, for_any)
+
+    result_df = operator(operation)
 
     return result_df
 
 
 if __name__ == "__main__":
-    path, input_files, output_file, operation, key_columns, for_any = getargs()
+    start = time()
     
-    result_df = operationResult( path, input_files, operation, key_columns, for_any)
+    path, input_files, output_file, operation, key_columns, for_any, dftype = getargs()  
+    result_df = operationResult(path, input_files, operation, key_columns, for_any, dftype)
     
     output_path = f'{path}/{output_file}'
-    savedf(result_df, output_path)
-
-    #print(result_df)
+    savedf(result_df, output_path, dftype)
+    
+    print(time()-start, 'sec')
+    
     #print(len(result_df))
+    #print(result_df.head())
