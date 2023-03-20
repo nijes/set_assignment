eb074c0ef4e2b24c092601ad279ee2fbf61c958d
nijes <monochrome05@gmail.com>
2023-03-20 18:04:27 +0900

    add dataio function by polars

--- setoperator/dataio.py
+++ setoperator/dataio.py
@@ -2,50 +2,53 @@ import polars as pl
 import pandas as pd
 
 
-def readdf(input_path:str) -> object: # 인자로 dftype 추가
-        file_extension = input_path.split('.')[-1]
-        # excel to df
-        if file_extension == 'xlsx':
-            return pl.read_excel(input_path)
-        # csv to df
-        elif file_extension == 'csv':
-            return pl.read_csv(input_path)
-        # tsv to df
-        elif file_extension == 'tsv':
-            return pl.read_csv(input_path, sep='\t')
-        # jsonl to df
-        elif file_extension == 'jsonl':
-            return pl.read_ndjson(input_path)
-        else:
-            raise Exception("지원하지 않는 파일 포맷")
-        # # excel to df
-        # if file_extension == 'xlsx':
-        #     return pd.read_excel(input_path, dtype="object")
-        # # csv to df
-        # elif file_extension == 'csv':
-        #     return pd.read_csv(input_path, dtype="object")
-        # # tsv to df
-        # elif file_extension == 'tsv':
-        #     return pd.read_csv(input_path, sep='\t', dtype="object")
-        # # jsonl to df
-        # elif file_extension == 'jsonl':
-        #     return pd.read_json(input_path, lines=True, dtype="object")
-        # else:
-        #     raise Exception("지원하지 않는 파일 포맷")
+def readdf(input_path:str, dftype:str) -> object:
+    file_extension = input_path.split('.')[-1]
 
-def savedf(df:object, output_path:str): # 인자로 dftype 추가
-    file_extension = output_path.split('.')[-1]
-    # df to excel
-    if file_extension == 'xlsx':
-        df.to_excel(output_path, index=False)
-    # df to csv
-    elif file_extension == 'csv':
-        df.to_csv(output_path, index=False)
-    # df to tsv
-    elif file_extension == 'tsv':
-        df.to_csv(output_path, sep='\t', index=False)
-    # df to jsonl
-    elif file_extension == 'jsonl':
-        df.to_json(output_path, orient='records', lines=True, force_ascii=False)
+    if dftype == 'polars':
+        # excel to df
+        if file_extension == 'xlsx': return pl.read_excel(input_path)
+        # csv to df
+        elif file_extension == 'csv': return pl.read_csv(input_path)
+        # tsv to df
+        elif file_extension == 'tsv': return pl.read_csv(input_path, sep='\t')
+        # jsonl to df
+        elif file_extension == 'jsonl': return pl.read_ndjson(input_path)
+        else: raise Exception("지원하지 않는 파일 포맷")
+    
     else:
-        raise Exception("지원하지 않는 파일 포맷")
+        # excel to df
+        if file_extension == 'xlsx': return pd.read_excel(input_path, dtype="object")
+        # csv to df
+        elif file_extension == 'csv': return pd.read_csv(input_path, dtype="object")
+        # tsv to df
+        elif file_extension == 'tsv': return pd.read_csv(input_path, sep='\t', dtype="object")
+        # jsonl to df
+        elif file_extension == 'jsonl': return pd.read_json(input_path, lines=True, dtype="object")
+        else: raise Exception("지원하지 않는 파일 포맷")
+
+
+
+def savedf(df:object, output_path:str, dftype:str): # 인자로 dftype 추가
+    file_extension = output_path.split('.')[-1]
+
+    if dftype == 'polars':
+        # df to excel
+        if file_extension == 'xlsx': df.write_excel(output_path)
+        # df to csv
+        elif file_extension == 'csv': df.write_csv(output_path)
+        # df to tsv
+        elif file_extension == 'tsv': df.write_csv(output_path, sep='\t')
+        # df to jsonl
+        elif file_extension == 'jsonl': df.write_ndjson(output_path)
+        else: raise Exception("지원하지 않는 파일 포맷")
+    else:
+        # df to excel
+        if file_extension == 'xlsx': df.to_excel(output_path, index=False)
+        # df to csv
+        elif file_extension == 'csv': df.to_csv(output_path, index=False)
+        # df to tsv
+        elif file_extension == 'tsv': df.to_csv(output_path, sep='\t', index=False)
+        # df to jsonl
+        elif file_extension == 'jsonl': df.to_json(output_path, orient='records', lines=True, force_ascii=False)
+        else: raise Exception("지원하지 않는 파일 포맷")
