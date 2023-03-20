65e1c7ccb07704dc7ef9dd65f79de2d1b1c6cd36
nijes <monochrome05@gmail.com>
2023-03-20 16:41:12 +0900

    operation condition by reduce

--- setoperator/setoperator.py
+++ setoperator/setoperator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from .dataio import readdf
+from functools import reduce
 
 
 class SetOperator:
@@ -25,42 +26,37 @@ class PandasOperator(SetOperator):
             raise SyntaxError
 
 
-    def _intersection_filter(self, main_kcol_set:set, sub_kcol_set:set) -> set:
-        if self.for_any:
-            intersection_filter_set = set()
-            for i in range(len(self.key_columns)):
-                intersection_filter_set.update(set(filter(lambda x: x[i] in set(map(lambda x: x[i], sub_kcol_set)), main_kcol_set)))
-        else:
-            intersection_filter_set = main_kcol_set & sub_kcol_set
+    def _intersection_condition(self, main_df:object, sub_df:object) -> object:
 
-        return intersection_filter_set
+        condition_list = []
+        for key_column in self.key_columns:
+            condition_list.append(main_df[key_column].isin(set(sub_df[key_column])))
+        
+        if self.for_any:
+            condition = reduce(lambda x, y: x | y, condition_list)
+        else:
+            condition = reduce(lambda x, y: x & y, condition_list)
+
+        return condition
         
 
 
     def _intersection(self) -> object:
-        main_df = readdf(input_path=f'{self.path}/{self.input_list[0]}')
-        intersection_filter_set = set(main_df[self.key_columns].apply(lambda x: tuple(x), axis=1))
+        result_df = readdf(input_path=f'{self.path}/{self.input_list[0]}')
 
         for input in self.input_list[1:]:
-            sub_df = readdf(input_path=f'{self.path}/{input}')[self.key_columns]
-            sub_kcol_set = set(sub_df.apply(lambda x: tuple(x), axis=1))
-            intersection_filter_set = self._intersection_filter(intersection_filter_set, sub_kcol_set)
+            sub_df = readdf(input_path=f'{self.path}/{input}')
+            result_df = result_df[self._intersection_condition(result_df, sub_df)]
 
-        result_df = main_df[main_df[self.key_columns].apply(lambda x: tuple(x), axis=1).isin(intersection_filter_set)]
-        
         return result_df
 
 
     def _diff(self) -> object:
-        main_df = readdf(input_path=f'{self.path}/{self.input_list[0]}')
-        diff_filter_set = set(main_df[self.key_columns].apply(lambda x: tuple(x), axis=1))
+        result_df = readdf(input_path=f'{self.path}/{self.input_list[0]}')
 
         for input in self.input_list[1:]:
-            sub_df = readdf(input_path=f'{self.path}/{input}')[self.key_columns]
-            sub_kcol_set = set(sub_df.apply(lambda x: tuple(x), axis=1))
-            diff_filter_set = diff_filter_set - self._intersection_filter(diff_filter_set, sub_kcol_set)
-
-        result_df = main_df[main_df[self.key_columns].apply(lambda x: tuple(x), axis=1).isin(diff_filter_set)]
+            sub_df = readdf(input_path=f'{self.path}/{input}')
+            result_df = result_df[~self._intersection_condition(result_df, sub_df)]
         
         return result_df       
 
@@ -70,19 +66,7 @@ class PandasOperator(SetOperator):
 
         for input in self.input_list[1:]:
             sub_df = readdf(input_path=f'{self.path}/{input}')
-            main_kcol_set = set(result_df[self.key_columns].apply(lambda x: tuple(x), axis=1))
-            sub_kcol_set = set(sub_df[self.key_columns].apply(lambda x: tuple(x), axis=1))  
-            sub_df = sub_df[~sub_df[self.key_columns].apply(lambda x: tuple(x), axis=1).isin(self._intersection_filter(sub_kcol_set, main_kcol_set))]
-            result_df = pd.concat([result_df, sub_df])[self.main_df.columns]
+            sub_df = sub_df[~self._intersection_condition(sub_df, result_df)][result_df.columns]            
+            result_df = pd.concat([result_df, sub_df])
 
         return result_df
-    
-
-
-class DaskOperator(SetOperator):
-    pass
-
-
-
-class PolarsOperator(SetOperator):
-    pass
