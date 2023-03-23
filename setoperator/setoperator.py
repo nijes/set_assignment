c18079fbae2dcf89831a9f178154368104d6b4e0
nijes <monochrome05@gmail.com>
2023-03-23 19:03:26 +0900

    add DuckdbOperator

--- setoperator/setoperator.py
+++ setoperator/setoperator.py
@@ -4,6 +4,7 @@ import dask.dataframe as dd
 import dask.delayed
 from .dataio import readdf
 from functools import reduce
+import duckdb
 
 
 
@@ -81,7 +82,7 @@ class PolarsOperator(SetOperator):
         self.dftype = 'polars'
 
 
-    def _intersection_condition(self, main_df:object, sub_df:object) -> object:
+    def _intersection_condition(self, sub_df:object) -> object:
         condition_list = []
         for key_column in self.key_columns:
             temp_set = list(map(lambda x: x[key_column], sub_df.select([key_column]).rows(named=True)))
@@ -100,7 +101,7 @@ class PolarsOperator(SetOperator):
 
         for input in self.input_list[1:]:
             sub_df = readdf(f'{self.path}/{input}', self.dftype)
-            result_df = result_df.filter(self._intersection_condition(result_df, sub_df))
+            result_df = result_df.filter(self._intersection_condition(sub_df))
 
         return result_df
 
@@ -110,7 +111,7 @@ class PolarsOperator(SetOperator):
 
         for input in self.input_list[1:]:
             sub_df = readdf(f'{self.path}/{input}', self.dftype)
-            result_df = result_df.filter(~self._intersection_condition(result_df, sub_df))
+            result_df = result_df.filter(~self._intersection_condition(sub_df))
         
         return result_df       
 
@@ -120,7 +121,7 @@ class PolarsOperator(SetOperator):
 
         for input in self.input_list[1:]:
             sub_df = readdf(f'{self.path}/{input}', self.dftype)
-            sub_df = sub_df.filter(~self._intersection_condition(sub_df, result_df))        
+            sub_df = sub_df.filter(~self._intersection_condition(result_df))        
             result_df = pl.concat([result_df, sub_df], how='diagonal').select(result_df.columns)
 
         return result_df
@@ -181,3 +182,42 @@ class DaskOperator(SetOperator):
             result_df = dd.concat([result_df, sub_df], axis=0)[result_df.columns]
 
         return result_df.compute()
+    
+
+
+class DuckdbOperator(SetOperator):
+    def __init__(self, path:str, input_list:list[str], key_columns:list[str], for_any:bool):
+        super().__init__(path, input_list, key_columns, for_any)
+        self.dftype = 'polars'
+
+
+    def _duckdb_operationResult(self, query_str:str) -> object:
+        result_df = readdf(f'{self.path}/{self.input_list[0]}', self.dftype)
+
+        for input in self.input_list[1:]:
+            sub_df = readdf(f'{self.path}/{input}', self.dftype)
+            result_df = duckdb.query(query_str).pl()
+
+        return result_df
+
+
+    def _intersection(self) -> object:       
+        connecting_condition = 'or' if self.for_any else 'and'
+        query_str = f'select * from result_df where' + \
+                    f'{connecting_condition}'.join(f' {key_column} in (select {key_column} from sub_df) ' for key_column in self.key_columns)        
+        return self._duckdb_operationResult(query_str)
+
+
+    def _diff(self) -> object:
+        connecting_condition = 'and' if self.for_any else 'or'
+        query_str = f'select * from result_df where' + \
+                    f'{connecting_condition}'.join(f' {key_column} not in (select {key_column} from sub_df) ' for key_column in self.key_columns)        
+        return self._duckdb_operationResult(query_str)      
+
+
+    def _union(self) -> object:
+        connecting_condition = 'and' if self.for_any else 'or'
+        query_str = f'select * from result_df union all ' + \
+                    f'select * from sub_df where' + \
+                    f'{connecting_condition}'.join(f' {key_column} not in (select {key_column} from result_df) ' for key_column in self.key_columns)
+        return self._duckdb_operationResult(query_str)
