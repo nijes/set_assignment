385e1309b21050cabfafe06b5119f83d4db528d7
nijes <monochrome05@gmail.com>
2023-03-23 19:01:57 +0900

    import DuckdbOperator

--- set_operations.py
+++ set_operations.py
@@ -1,4 +1,5 @@
 from setoperator import getargs, savedf, PandasOperator, PolarsOperator, DaskOperator
+from setoperator import DuckdbOperator
 from time import time
 #from dask.distributed import Client, progress, LocalCluster
 import dask
@@ -13,6 +14,8 @@ def operationResult(path:str, input_list:list[str], operation:str, key_columns:l
         #client = Client(n_workers=8, threads_per_worker=2, memory_limit = '16GB')
         dask.config.set(scheduler='threads', num_workers=8)
         operator = DaskOperator(path, input_list, key_columns, for_any)
+    elif dftype == 'duckdb':
+        operator = DuckdbOperator(path, input_list, key_columns, for_any)
         
     result_df = operator(operation)
 
@@ -21,15 +24,12 @@ def operationResult(path:str, input_list:list[str], operation:str, key_columns:l
 
 if __name__ == "__main__":
     start = time()
-    #client = Client()
 
-    path, input_files, output_file, operation, key_columns, for_any, dftype = getargs()  
-    
+    path, input_files, output_file, operation, key_columns, for_any, dftype = getargs()
     result_df = operationResult(path, input_files, operation, key_columns, for_any, dftype)
 
     output_path = f'{path}/{output_file}'
     
-    #print(time()-start, 'start saving...')
     savedf(result_df, output_path, dftype)
     print(time()-start, 'sec')
 
