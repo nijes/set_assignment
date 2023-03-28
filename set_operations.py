7a41c84aea87cd089fd839b52b1d306f30cb5ef0
nijes <monochrome05@gmail.com>
2023-03-28 11:53:26 +0900

    edit function name and args

--- set_operations.py
+++ set_operations.py
@@ -1,38 +1,35 @@
-from setoperator import getargs, savedf
+from setoperator import getargs, readdfs, savedf
 from setoperator import PandasOperator, PolarsOperator, DaskOperator, DuckdbOperator
 import dask
 
 
-def operationResult(
-    path: str,
-    input_list: list[str],
+def setoperation(
+    df_list: list[object],
     operation: str,
     key_columns: list[str],
     for_any: bool,
     dftype: str,
 ) -> object:
     if dftype == "pandas":
-        operator = PandasOperator(path, input_list, key_columns, for_any)
+        operator = PandasOperator(df_list, key_columns, for_any)
     elif dftype == "polars":
-        operator = PolarsOperator(path, input_list, key_columns, for_any)
+        operator = PolarsOperator(df_list, key_columns, for_any)
     elif dftype == "dask":
         dask.config.set(scheduler="threads", num_workers=8)
-        operator = DaskOperator(path, input_list, key_columns, for_any)
+        operator = DaskOperator(df_list, key_columns, for_any)
     elif dftype == "duckdb":
-        operator = DuckdbOperator(path, input_list, key_columns, for_any)
-
+        operator = DuckdbOperator(df_list, key_columns, for_any)
     result_df = operator(operation)
     return result_df
 
 
 if __name__ == "__main__":
     args = getargs()
-    result_df = operationResult(
-        args.path,
-        args.input_files,
-        args.operation,
-        args.key_columns,
-        args.for_any,
-        args.dftype,
+    result_df = setoperation(
+        df_list=readdfs(args.path, args.input_files, args.dftype),
+        operation=args.operation,
+        key_columns=args.key_columns,
+        for_any=args.for_any,
+        dftype=args.dftype,
     )
     savedf(result_df, f"{args.path}/{args.output_file}", args.dftype)
